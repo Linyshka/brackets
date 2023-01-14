6ea4773c624a2b8b334b2d03d5914323164bcc08
Алина Радюкевич <alina.makaroshka@outlook.com>
2023-01-14 21:26:28 +0300

    feat: realize function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let n = 0;
+  while(n < str.length) {
+    if(str[n] === "(")
+      stack.push("(");
+    else if(str[n] === "{")
+      stack.push("{");
+    else if(str[n] === "[")
+      stack.push("[");
+    else if(str[n] === "1")
+      stack.push("1");
+    else if(str[n] === "3")
+      stack.push("3");
+    else if(str[n] === "5")
+      stack.push("5");
+    else if(str[n] === ")") {
+      if(stack.pop() != "(")
+        return false;
+    }
+    else if(str[n] === "}") {
+      if(stack.pop() != "{")
+        return false;
+    }
+    else if(str[n] === "]") {
+      if(stack.pop() != "[")
+        return false;
+    }
+    else if(str[n] === "2") {
+      if(stack.pop() != "1")
+        return false;
+    }
+    else if(str[n] === "4") {
+      if(stack.pop() != "3")
+        return false;
+    }
+    else if(str[n] === "6") {
+      if(stack.pop() != "5")
+        return false;
+    }
+    else if(str[n] === "7") {
+      if(stack[stack.length - 1] === "7")
+        stack.pop();
+      else stack.push("7");
+    }
+    else if(str[n] === "8") {
+      if(stack[stack.length - 1] === "8")
+        stack.pop();
+      else stack.push("8");
+    }
+    else if(str[n] === "|") {
+      if(stack[stack.length - 1] === "|")
+        stack.pop();
+      else stack.push("|");
+    }
+    n++;
+  }
+  if(stack.length)
+    return false;
+  return true;
 }
